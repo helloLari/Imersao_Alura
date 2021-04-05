32c73de8ad541448835da4919fa278702d8d3a82
larrisse <larisse1991@gmail.com>
2021-04-04 22:21:04 -0300

    imersão alura

--- Dia_1/script.js
+++ Dia_1/script.js
@@ -1,6 +1,6 @@
 let valorDolarText = prompt("Valor em dólar:")
 let valorDolarNumber = parseFloat(valorDolarText) // transforma oq foi digitado para numero
 
-let valorReal = valorDolarNumber * 5.50
-let valorRealFixed = valorReal.toFixed(2)
+let valorReal = valorDolarNumber * 5.50 // faz a conversão do valor digitado
+let valorRealFixed = valorReal.toFixed(2) // retorna em casas decimais o calculo
 alert(valorRealFixed)
